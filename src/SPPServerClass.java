273da68c3670b8474bfde119e327fc73b73fbce3
inesbouhajeb <i.bouhajeb@gmail.com>
2018-04-02 13:25:21 +0200

    Completed GUI with Server

--- src/SPPServerClass.java
+++ src/SPPServerClass.java
@@ -1,6 +1,4 @@
-import javafx.scene.image.Image;
-import javax.bluetooth.BluetoothStateException;
-import javax.bluetooth.LocalDevice;
+import javafx.application.Platform;
 import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.UUID;
 import javax.microedition.io.Connector;
@@ -11,19 +9,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * Code from: http://www.jsr82.com/jsr-82-sample-spp-server-and-client/
+ * Based on code from: http://www.jsr82.com/jsr-82-sample-spp-server-and-client/
  */
 
-public class SPPServerClass {
+public class SPPServerClass{
 
-    private String connectionString;
     private StreamConnectionNotifier mStreamConnectionNotifier;
-    private StreamConnection connection;
-    private boolean scannerRunning=true;
-    private Image imageFromBytes;
 
-    public void startServer() {
-        //1. Constructing the connection URL for StreamConnectionNotifier. Only used to open it.
+    public void startServer()  {
+        String connectionString;
+
+        String fileName;
+
+        //1. Constructing the connection URL for StreamConnectionNotifier. Only used to open it. We don't use it in Android app.
 
         //Create a UUID for SPP
         UUID uuid=new UUID("1101",true);
@@ -33,83 +31,70 @@ public class SPPServerClass {
                 + uuid
                 +";name=Sample SPP Server";
 
-
     //2. Registering service and Waiting for Client connection.
+
         //open server url
         try {
             mStreamConnectionNotifier = (StreamConnectionNotifier) Connector.open(connectionString);
-            //Wait for client connection
 
-            System.out.println("\nServer Started. Waiting for clients to connect…");
-            connection=mStreamConnectionNotifier.acceptAndOpen();//The acceptAndOpen method waits until a client is connected.
+            //Waits for client connection
+            ServerFX.getTextArea().appendText("Server Started. Waiting for clients to connect…\n\n");
+            try {
+                StreamConnection connection = mStreamConnectionNotifier.acceptAndOpen();//The acceptAndOpen method waits until a client is connected.
+                if(connection!=null){
 
-            RemoteDevice dev=RemoteDevice.getRemoteDevice(connection);
-            System.out.println("Remote device address: "+dev.getBluetoothAddress());
-            System.out.println("Remote device name: "+dev.getFriendlyName(true));
+                    RemoteDevice dev=RemoteDevice.getRemoteDevice(connection);
+                    ServerFX.getStopBtn().setVisible(false);
+                    ServerFX.getTextArea().appendText("Connection started. Please wait...\n");
+                    ServerFX.getTextArea().appendText("Remote device address: "+dev.getBluetoothAddress()+"\n");
+                    ServerFX.getTextArea().appendText("Remote device name: "+dev.getFriendlyName(true)+"\n");
 
-            //read data from spp client using InputStream
+                    //TODO
+                    //read data from spp client using InputStream
+                    int size=240664; //for now, later change it to check amount of bytes received from client
+                    InputStream inputStream=connection.openInputStream();
+                    size=inputStream.available();
+                    byte[] receivedBytes= new byte[size];
+                    inputStream.read(receivedBytes,0,size);
 
-            int size=240664; //for now, later change it to check amount of bytes received from client //TODO
-            InputStream inputStream=connection.openInputStream();
-            size=inputStream.available();
-            byte[] receivedBytes= new byte[size];
-            inputStream.read(receivedBytes,0,size);
+                    //for me to see comming bytes
+                    for(int i=0;i<receivedBytes.length;i++){
+                        System.out.println(receivedBytes[i]);
+                    }
 
-            //for me to see comming bytes
-            for(int i=0;i<receivedBytes.length;i++){
-                System.out.println(receivedBytes[i]);
+                    ServerFX.getTextArea().appendText(" Length of received bytes: "+receivedBytes.length+"\n");
+
+                    //saving bytes as file
+                    String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                    fileName="ImgBluetoothApp_"+timeStamp+"_.jpeg";
+                    FileOutputStream fos=new FileOutputStream(fileName);
+                    fos.write(receivedBytes);
+                    fos.close();
+
+                    mStreamConnectionNotifier.close();
+                    ServerFX.getTextArea().appendText("Success! Your image was saved.\n");
+                    ServerFX.getImageBtn().setVisible(true);
+                    ServerFX.openImage(fileName);
+                    Platform.runLater(()->{
+                        ServerFX.getStopBtn().setText("Run again");
+                    });
+                    ServerFX.getStopBtn().setVisible(true);
+                }
             }
-            System.out.println(" Length of received bytes: "+receivedBytes.length);
-
-            //saving bytes as file
-            String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-            FileOutputStream fos=new FileOutputStream("ImgBluetoothApp_"+timeStamp+"_.jpeg");
-            fos.write(receivedBytes);
-            fos.close();
-
-            //TODO
-            //setting image in GUI
-            //imageFromBytes = new Image(new ByteArrayInputStream(receivedBytes));
-
-            //send response to spp client, not needed now
-//            OutputStream outStream=connection.openOutputStream();
-//            PrintWriter pWriter=new PrintWriter(new OutputStreamWriter(outStream));
-//            pWriter.write("Response String from SPP Server\r\n");
-//            pWriter.flush();
-
-            scannerRunning=false;
-
-//            pWriter.close();
-            mStreamConnectionNotifier.close();
+            catch (InterruptedIOException e){
+                e.getStackTrace();
+            }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            Platform.runLater(()->{
+                ServerFX.showAlert();
+                ServerFX.getStopBtn().setText("Run again");
+                ServerFX.getStopBtn().setVisible(true);
+            });
         }
     }
 
-    public static void main(String[] args) throws IOException {
-
-//display local device address and name
-        try {
-            LocalDevice localDevice = LocalDevice.getLocalDevice();
-            System.out.println("Address: " + localDevice.getBluetoothAddress());
-            System.out.println("Name: " + localDevice.getFriendlyName());
-
-            SPPServerClass mSppServerClass = new SPPServerClass();
-            mSppServerClass.startServer();
-        }
-        catch (BluetoothStateException e)
-        {
-            System.out.println("BluetootStack not detected! Turn on your Bluetooth mode on your device!");
-        }
-
-    }
-
-    public boolean isScannerRunning() {
-        return scannerRunning;
-    }
-
-    public Image getImageFromBytes() {
-        return imageFromBytes;
+    public StreamConnectionNotifier getmStreamConnectionNotifier() {
+        return mStreamConnectionNotifier;
     }
 }
--- src/ServerFX.java
+++ src/ServerFX.java
@@ -1,66 +1,159 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.LocalDevice;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import java.util.Scanner;
-
-public class ServerFX extends Application implements Runnable{
+public class ServerFX extends Application{
 
     private Button startBtn;
-    private TextArea textArea;
-    private ImageView imageView;
-    private Scanner scanner;
-    private SPPServerClass server;
+    private static Button stopBtn;
+    private static Button imageBtn;
+
+    private static TextArea textArea;
+    private SPPServerClass mSppServerClass;
+    private VBox vBox;
+    private HBox stopHbox;
+    private HBox startHbox;
+
+    private Thread mThread;
+    private LocalDevice localDevice;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
         primaryStage.setTitle("SPP Server Bluetooth Application");
 
-        server=new SPPServerClass();
-
-        scanner=new Scanner(System.in);
-
-        textArea=new TextArea();
-        textArea.setEditable(false);
-        imageView=new ImageView();
         startBtn=new Button("Start server");
+        stopBtn=new Button("Stop server");
+        imageBtn=new Button("Open image");
 
+        textArea=new TextArea("");
+        textArea.setEditable(false);
+        textArea.setVisible(false);
+        textArea.setPrefHeight(200);
 
+        vBox=new VBox();
+        vBox.setSpacing(10);
+        vBox.getChildren().add(textArea);
+        stopHbox=new HBox(stopBtn);
+        startHbox=new HBox(startBtn);
+        startHbox.setAlignment(Pos.CENTER);
+        stopHbox.setAlignment(Pos.CENTER);
+        vBox.getChildren().add(startHbox);
+        vBox.getChildren().add(imageBtn);
+        imageBtn.setVisible(false);
+        vBox.setAlignment(Pos.CENTER);
 
-        startBtn.setOnAction(e->{
+        stopBtn.setOnAction(e->{
 
-            server.startServer();
-
-            imageView.setImage(server.getImageFromBytes());
+            ServerFX.getStopBtn().setText("Stop server");
+            imageBtn.setVisible(false);
+            textArea.clear();
+            vBox.getChildren().remove(stopHbox);
+            vBox.getChildren().remove(imageBtn);
+            vBox.getChildren().add(startHbox);
+            vBox.getChildren().add(imageBtn);
+            imageBtn.setVisible(false);
+            textArea.setVisible(false);
         });
 
-        HBox hBox=new HBox(startBtn,textArea);
-        VBox vBox=new VBox();
-        vBox.getChildren().add(hBox);
-        vBox.getChildren().add(imageView);
+        startBtn.setOnAction(e->{
+            getStopBtn().setText("Stop Server");
 
-        Scene scene=new Scene(vBox);
+            try {
+                localDevice = LocalDevice.getLocalDevice();
+
+                if(localDevice.getFriendlyName()!=null){
+                    vBox.getChildren().remove(startHbox);
+                    vBox.getChildren().remove(imageBtn);
+                    vBox.getChildren().add(stopHbox);
+                    vBox.getChildren().add(imageBtn);
+                    textArea.setVisible(true);
+
+                    //shows current time
+                    Date date=new Date();
+                    SimpleDateFormat sdf=new SimpleDateFormat("HH:mm:ss");
+                    textArea.appendText("Started at: "+sdf.format(date)+"\n\n");
+                    textArea.appendText("Local device address: " + localDevice.getBluetoothAddress()+"\n");
+                    textArea.appendText("Local device name: " + localDevice.getFriendlyName()+"\n");
+                }
+
+                runServerInThread();
+            }
+            catch (BluetoothStateException exception)
+            {
+                showAlert();
+            }
+
+        });
+
+        Scene scene=new Scene(vBox,500,300);
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch(args);
-        (new Thread(new ServerFX())).start();
     }
 
-    @Override
-    public void run() {
-        while(server.isScannerRunning()){
-            textArea.setText(scanner.nextLine());
-        }
+    /**
+     * SPPServerClass takes a long time to run so we run it in a background thread using runServerInThread.
+     */
+
+    private void runServerInThread(){
+
+        mThread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mSppServerClass = new SPPServerClass();
+                 mSppServerClass.startServer();
+                }
+        });
+        mThread.start();
+    }
+
+    public static TextArea getTextArea() {
+        return textArea;
+    }
+
+    public static void showAlert(){
+        Alert alert=new Alert(Alert.AlertType.WARNING);
+        alert.setHeaderText("BluetoothStack not detected!");
+        alert.setContentText("Turn on your Bluetooth mode on your server device!");
+        alert.showAndWait();
+    }
+
+    public static Button getImageBtn() {
+        return imageBtn;
+    }
+
+    public static void openImage(String fileName){
+        imageBtn.setOnAction(event -> {
+            File file=new File(fileName);
+            try {
+                Desktop.getDesktop().open(file);
+            } catch (IOException | IllegalArgumentException e) {
+                Alert alert=new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText("Failed opening file!");
+                alert.setContentText("Image doesn't exist.");
+                alert.showAndWait();
+            }
+        });
+    }
+
+    public static Button getStopBtn() {
+        return stopBtn;
     }
 }
