f43205c3f9817b16c4d4e2e259bc7bae6826f9ba
inesbouhajeb <i.bouhajeb@gmail.com>
2018-04-05 18:17:57 +0200

    Completed in a part

--- src/SPPServerClass.java
+++ src/SPPServerClass.java
@@ -1,19 +1,12 @@
-import com.sun.glass.ui.Pixels;
 import javafx.application.Platform;
 import javax.bluetooth.RemoteDevice;
 import javax.bluetooth.UUID;
-import javax.imageio.ImageIO;
 import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 import javax.microedition.io.StreamConnectionNotifier;
-import javax.swing.text.html.HTMLDocument;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.*;
-import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Iterator;
 
 /**
  * Based on code from: http://www.jsr82.com/jsr-82-sample-spp-server-and-client/
@@ -25,8 +18,6 @@ public class SPPServerClass{
 
     public void startServer()  {
         String connectionString;
-
-        String fileName;
 
         //1. Constructing the connection URL for StreamConnectionNotifier. Only used to open it. We don't use it in Android app.
 
@@ -56,44 +47,27 @@ public class SPPServerClass{
                     ServerFX.getTextArea().appendText("Remote device address: "+dev.getBluetoothAddress()+"\n");
                     ServerFX.getTextArea().appendText("Remote device name: "+dev.getFriendlyName(true)+"\n");
 
-                    //TODO/
                     // read data from spp client using InputStream
-                    int size=65536;//31961088;
-                    byte[] receivedBytes= new byte[size];
+                    ByteArrayOutputStream baos=new ByteArrayOutputStream();
                     InputStream connectionInputStream=connection.openInputStream();
-                    System.out.println("Avaible bytes from inputStream: "+connectionInputStream.available());
-                    //handler
-                    connectionInputStream.read(receivedBytes,0,size);
-                    connectionInputStream.close();//zamkykamy inputa, ktorym pobralismy bity
-                    // for me to see comming bytes
-                    for(int i=0;i<receivedBytes.length;i+=1000){
-                        System.out.println(receivedBytes[i]);
+                    int nRead=0;
+                    byte[] testData=new byte[1024];
+                    while ((nRead = connectionInputStream.read(testData, 0, testData.length)) != -1) {
+                        baos.write(testData,0,nRead);
                     }
-
-                    //saving bytes and conversion
-                    InputStream in=new ByteArrayInputStream(receivedBytes);//input streamem sa bity
-                    BufferedImage img= ImageIO.read(in);//it could return NULL
-                    Iterator i=ImageIO.getImageReaders(in);
-                    if(i.hasNext()){
-                        System.out.println("Reader is found");
-                    }
-                    else {
-                        System.out.println("Reader is not found");
-                    }
-                    if(img==null){
-                        System.out.println("Image is NULL");
-                    }
-
-                    File outputFile=new File("saved.jpg");
-                    ImageIO.write(img,"jpg",outputFile);
-                    System.out.println("Image created!");
-                    in.close();
-
+                    baos.flush();
+                    byte[] endlessByteArray=baos.toByteArray();
+                    baos.close();
+                    connectionInputStream.close();
                     //file stamp
                     String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-                    fileName="ImgBluetoothApp_"+timeStamp+"_.jpeg";
-
+                    String fileName="ImgBluetoothApp_"+timeStamp+"_.jpeg";
+                    //saving to the file
+                    FileOutputStream fos=new FileOutputStream(fileName);
+                    fos.write(endlessByteArray);
+                    fos.close();
                     mStreamConnectionNotifier.close();
+
                     ServerFX.getTextArea().appendText("Success! Your image was saved.\n");
                     ServerFX.getImageBtn().setVisible(true);
                     ServerFX.openImage(fileName);
@@ -101,7 +75,6 @@ public class SPPServerClass{
                         ServerFX.getStopBtn().setText("Run again");
                     });
                     ServerFX.getStopBtn().setVisible(true);
-                    in.close();
                 }
             }
             catch (InterruptedIOException e){
@@ -115,9 +88,5 @@ public class SPPServerClass{
                 ServerFX.getStopBtn().setVisible(true);
             });
         }
-    }
-
-    public StreamConnectionNotifier getmStreamConnectionNotifier() {
-        return mStreamConnectionNotifier;
     }
 }
